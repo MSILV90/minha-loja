4e687a29b61b79385d8bd300bf89b1308e289173
Maria de Fatima Alves <maria.fatima_alves@hotmail.com>
2021-12-27 02:49:54 -0300

    commit inicial

---
+++ app/src/main/java/foo/exercicio/appminhaloja/MainActivity.kt
@@ -0,0 +1,17 @@
+package foo.exercicio.appminhaloja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sairDaLoja(view: android.view.View) {
+
+
+        finish()
+    }
+}
